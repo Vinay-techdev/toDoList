5398c908e43f25056517d7ee64706f0a464a2189
vinay techdev <vinayvinuu629@gmail.com>
2025-03-14 18:36:07 +0530

    adding

---
+++ README.md
@@ -0,0 +1,7 @@
+# To Do List
+
+ To Do List website using React.js
+
+# About
+
+User can add, delete and move items up and down
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ToDoList from "./ToDolist";
+
+function App() {
+
+    return(
+      <>
+         <ToDoList />
+      </>
+    );
+}
+export default App
